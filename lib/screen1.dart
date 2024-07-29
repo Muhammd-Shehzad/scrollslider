f5be39e12620a4a4f3046149799a92aff66465f5
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-07-29 21:06:16 +0500

    first commit

---
+++ lib/screen1.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatefulWidget {
+  const Screen1({super.key});
+
+  @override
+  State<Screen1> createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text(
+            'Zu App',
+            style: TextStyle(
+                color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 80,
+              width: 500,
+              decoration: BoxDecoration(color: Colors.grey),
+              child: Center(
+                child: Text(
+                  'Welcom,Shehzad',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 300,
+                  width: 500,
+                  decoration: BoxDecoration(color: Colors.amber),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
